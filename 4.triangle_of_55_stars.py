f3474ccff76dee1e54c3f39647296a8f517846b8
me_madness <p_rangel@abv.bg>
2023-07-22 23:03:09 +0200

    first commit

---
+++ 4.triangle_of_55_stars.py
@@ -0,0 +1,11 @@
+def triangle(s):
+    
+    for i in range(0, s):
+        
+        for j in range(0, i+1):
+            
+            print("* ",end="")
+            
+        print("\r")
+s = 10
+triangle(s)
---
+++ 6.square_of_stars.py
@@ -0,0 +1,10 @@
+# No of rows
+n = 5
+
+# Loop through rows
+for i in range(n):
+    
+    # Loop to print pattern
+    for j in range(n):
+        print("*", end=' ')
+    print()
